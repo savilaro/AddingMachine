64e0db5d22e46130e2d49a506b5a69d40489936d
savilaro <savilaro@asu.edu>
2020-10-07 19:58:26 -0700

    Second version - implemented all methods and tested 2 calculations.

--- cse360assignment01/src/cse360assignment01/AddingMachine.java
+++ cse360assignment01/src/cse360assignment01/AddingMachine.java
@@ -6,7 +6,8 @@ package cse360assignment01;
  */
 public class AddingMachine {
 	 private int total;
-	  
+	 private String transactions = "0";
+	 
 	 /**
 	  * AddingMachine default constructor
 	  */
@@ -18,29 +19,54 @@ public class AddingMachine {
 	   * @return the current total
 	   */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  /**
 	   * This adds to the total variable
 	   * @param value the number to add to the total
 	   */
 	  public void add (int value) {
+		  total = total + value;
+		  transactions = transactions + " + " + value;
 	  }
 	  /**
 	   * This subtracts from the total variable
 	   * @param value the number to subtract from the total
 	   */
 	  public void subtract (int value) {
+		  total = total - value;
+		  transactions = transactions + " - " + value;
 	  }
 	  /**
-	   * This returns all the calculations from adding and subtracting to the initial total 0 
+	   * This returns the history from adding and subtracting to the initial total 0 
 	   */
 	  public String toString () {
-	    return "";
+	    return transactions;
 	  }
 	  /**
-	   * This clears the AddingMachine
+	   * This clears the AddingMachine and history of calculations
 	   */
 	  public void clear() {
+		  total = 0;
+		  transactions = "0";
 	  }
+	  
+	  public static void main (String [] args) {
+		  System.out.println("TEST");
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		System.out.println(myCalculator);
+		System.out.println("Total = " + myCalculator.getTotal());
+		myCalculator.clear();
+		myCalculator.add(12);
+		myCalculator.subtract(4);
+		myCalculator.add(8);
+		myCalculator.add(10);
+		myCalculator.add(3);
+		myCalculator.subtract(19);
+		System.out.println(myCalculator);
+		System.out.println("Total = " + myCalculator.getTotal());
+		}
 }
