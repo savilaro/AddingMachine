df11e4bbf53b8d834a3ff42a0dc9118d6b347061
savilaro <savilaro@asu.edu>
2020-10-07 18:00:29 -0700

    First commit with Javadoc comments and empty methods.

---
+++ cse360assignment01/src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,46 @@
+package cse360assignment01;
+
+/**
+ * This class adds or subtracts to get a total.
+ * @author Sandra Avila Robledo
+ */
+public class AddingMachine {
+	 private int total;
+	  
+	 /**
+	  * AddingMachine default constructor
+	  */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  /**
+	   * This returns the current total from adding/subtracting to a value
+	   * @return the current total
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  /**
+	   * This adds to the total variable
+	   * @param value the number to add to the total
+	   */
+	  public void add (int value) {
+	  }
+	  /**
+	   * This subtracts from the total variable
+	   * @param value the number to subtract from the total
+	   */
+	  public void subtract (int value) {
+	  }
+	  /**
+	   * This returns all the calculations from adding and subtracting to the initial total 0 
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  /**
+	   * This clears the AddingMachine
+	   */
+	  public void clear() {
+	  }
+}
